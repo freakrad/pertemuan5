c1a8eb032585c3de77d7221c16896ed1f17712de
freakrad <fachrad.za@gmail.com>
2024-11-29 09:15:38 +0700

    push 1

---
+++ src/main/java/com/mahasiswa/repository/MahasiswaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mahasiswa.repository;
+
+import com.mahasiswa.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Permata
+ */
+@Repository
+
+public interface MahasiswaRepository  extends JpaRepository<ModelMahasiswa, Long>{
+
+}
+
